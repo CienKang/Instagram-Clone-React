4c7162f29ac1df231b9b039ae0271ae8f9deba72
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2021-04-22 10:30:32 +0530

    Setting up tailwind.css and Starting Login Page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import {firebase, FieldValue} from './lib/firebase';
-
+import './styles/app.css'
 
 ReactDOM.render(
     <FirebaseContext.Provider value = {{firebase, FieldValue}}>
@@ -11,3 +11,5 @@ ReactDOM.render(
     </FirebaseContext.Provider>,  
     document.getElementById('root')
 );
+
+//  Start from 1:36:49 fcc
